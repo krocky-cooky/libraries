25d659faecf03f4677c6a7962214178554039fda
krocky-cooky <64895404+krocky-cooky@users.noreply.github.com>
2020-10-23 00:30:01 +0900

    update rmq

--- RMQ.cpp
+++ RMQ.cpp
@@ -1,14 +1,31 @@
+//区間最大値または最小値を求めるセグメント木の構造体
+
 template <typename T>
 struct RMQ {
     const T INF = numeric_limits<T>::max();
+    T bound;  //最小値のときはinf,それ以外の時0となる
     int n;
+    bool flag; //最大値でtrue
     vector<T> dat;
-    RMQ(int n_) : n(), dat(n_ * 4,INF){
+    RMQ(int n_,bool flag_ = true) : n(),flag(flag_), dat(n_ * 4,INF){
         int x = 1;
         while(n_ > x){
             x *= 2;
         }
         n = x;
+        if(flag_){
+            bound = 0;
+        }else{
+            bound = INF;
+        }
+    }
+
+    T func(T a,T b){
+        if(flag){
+            return max(a,b);
+        }else{
+            return min(a,b);
+        }
     }
 
     void update(int i,T x){
@@ -16,7 +33,7 @@ struct RMQ {
         dat[i] = x;
         while(i > 0){
             i = (i-1)/2;
-            dat[i] = min(dat[i*2+1],dat[i*2+2]);
+            dat[i] = func(dat[i*2+1],dat[i*2+2]);
         }
     }
 
@@ -27,15 +44,15 @@ struct RMQ {
 
     T query_sub(int a,int b,int k,int l,int r){
         if(r <= a || b <= l){
-            return INF;
+            return bound;
         }else if(a <= l && r <= b){
             return dat[k];
         }else{
-            T vl = query_sub(a, b, k * 2 + 1, l, (l + r) / 2);
-            T vr = query_sub(a, b, k * 2 + 2, (l + r) / 2, r);
-            return min(vl, vr);
+            T v_l = query_sub(a, b, k * 2 + 1, l, (l + r) / 2);
+            T v_r = query_sub(a, b, k * 2 + 2, (l + r) / 2, r);
+            return func(v_l, v_r);
         }
     }
 
 
-}
+};
