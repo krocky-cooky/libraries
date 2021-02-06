03d801d760b2ee122dc6bab4272c4db4599917af
krocky-cooky <64895404+krocky-cooky@users.noreply.github.com>
2021-02-06 23:55:46 +0900

    djikstra

--- djikstra.cpp
+++ djikstra.cpp
@@ -1,27 +1,33 @@
+/*
+エッジの重みが正の場合にのみ、最短経路を探索することができる
+
+
+
+→未確定ノードのうち距離が最小のノードを確定とする
+→そのノードからいけるノードをqueueに追加する
+*/  
+
 struct edge{
     ll from,to,cost;
 };
 
-struct dikstra{
+struct dijkstra{
     ll n;
     vector<ll>dist;
-    vector<bool>done;
     vector<vector<edge>>es;
-    dikstra(ll _n):n(_n),dist(_n,1e18),done(_n,false),es(_n){}
+    dijkstra(ll _n):n(_n),dist(_n,1e18),done(_n,false),es(_n){}
     void add(ll from,ll to,ll cost){
         es[from].pb({from,to,cost});
     }
     void search(ll x){
         priority_queue<pair<ll,ll>>que;
         que.push({0,x});
-        done[x] = true;
         dist[x] = 0;
         while(!que.empty()){
             auto k = que.top();que.pop();
             ll d = -k.first,i = k.second;
             if(d > dist[i])continue;
             for(edge e : es[i]){
-                if(done[e.to])continue;
                 if(dist[e.to] > d + e.cost){
                     dist[e.to] = d + e.cost;
                     que.push({-dist[e.to],e.to});
