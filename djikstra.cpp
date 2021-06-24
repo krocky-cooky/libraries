65ad3505db0d924c6c0e8e06825b3b1eeb588e3f
krocky-cooky <64895404+krocky-cooky@users.noreply.github.com>
2021-06-24 15:35:01 +0900

    update dijkstra

--- djikstra.cpp
+++ djikstra.cpp
@@ -15,22 +15,22 @@ struct dijkstra{
     ll n;
     vector<ll>dist;
     vector<vector<edge>>es;
-    dijkstra(ll _n):n(_n),dist(_n,1e18),done(_n,false),es(_n){}
+    dijkstra(ll _n):n(_n),dist(_n,1e18),es(_n){}
     void add(ll from,ll to,ll cost){
         es[from].pb({from,to,cost});
     }
     void search(ll x){
-        priority_queue<pair<ll,ll>>que;
+        priority_queue<pair<ll,ll>,vector<pair<ll,ll>>,greater<pair<ll,ll>>>que;
         que.push({0,x});
         dist[x] = 0;
         while(!que.empty()){
             auto k = que.top();que.pop();
-            ll d = -k.first,i = k.second;
+            ll d = k.first,i = k.second;
             if(d > dist[i])continue;
             for(edge e : es[i]){
                 if(dist[e.to] > d + e.cost){
                     dist[e.to] = d + e.cost;
-                    que.push({-dist[e.to],e.to});
+                    que.push({dist[e.to],e.to});
                 }
             }
         }
